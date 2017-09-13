6dc089020dbcc9f4c6381ce7c4f1aac288efdc2b
马三小伙儿 <1790617178@qq.com>
2017-09-13 22:54:26 +0800

    自动注入框架

---
+++ Assets/Scripts/AutoInject/AutoInjectAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+
+
+/// <summary>
+/// 自动注入标签（打上该标签的都可以参与自动注入）
+/// </summary>
+public class AutoInjectAttribute : Attribute
+{
+    public string name { set; get; }
+    public Type type { set; get; }
+    public FieldInfo fi { set; get; }
+    public AutoInjectAttribute(string name = null)
+    {
+        this.name = name;
+    }
+}
