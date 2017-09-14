aaaab5568be69e046bc5a702a7b35be39bdfdb5b
马三小伙儿 <1790617178@qq.com>
2017-09-14 20:55:46 +0800

    本地表数据的抽象基类

---
+++ Assets/Scripts/Data/LocalDataBase.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 本地数据表中的单条数据的基类
+/// </summary>
+public abstract class LocalDataBase
+{
+    /// <summary>
+    /// ID
+    /// </summary>
+    public int id;
+
+    /// <summary>
+    /// 初始化数据
+    /// </summary>
+    /// <param name="strData"></param>
+    public abstract void InitWithStr(string strData, char splitChar = ',');
+
+    /// <summary>
+    /// 得到Int数据
+    /// </summary>
+    /// <param name="str"></param>
+    /// <returns></returns>
+    protected int GetInt(string str)
+    {
+        if (!string.IsNullOrEmpty(str))
+        {
+            return int.Parse(str);
+        }
+
+        return 0;
+    }
+
+    /// <summary>
+    /// 得到float数据
+    /// </summary>
+    /// <param name="str"></param>
+    /// <returns></returns>
+    protected float GetFloat(string str)
+    {
+        if (!string.IsNullOrEmpty(str))
+        {
+            return float.Parse(str);
+        }
+
+        return 0;
+    }
+
+    /// <summary>
+    /// 得到bool数据
+    /// </summary>
+    /// <param name="str"></param>
+    /// <returns></returns>
+    protected bool GetBool(string str)
+    {
+        if (!string.IsNullOrEmpty(str))
+        {
+            return bool.Parse(str);
+        }
+
+        return false;
+    }
+}
+
+/// <summary>
+/// DataMap，数据集合的接口，实现该接口用于管理LocalDataBase
+/// </summary>
+public interface ILocalDataMapBase
+{
+    /// <summary>
+    /// 对多行字符串进行处理，保存成LocalDataBase集合
+    /// </summary>
+    /// <param name="rows"></param>
+    void SetMapCsv(string[] rows);
+}
