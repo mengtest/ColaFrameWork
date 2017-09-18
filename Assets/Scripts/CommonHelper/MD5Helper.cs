b8436ba35596e7a7d7774efaef3a004650f2225a
马三小伙儿 <1790617178@qq.com>
2017-09-18 21:41:24 +0800

    修改命名

---
+++ Assets/Scripts/CommonHelper/MD5Helper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// MD5码加密工具类
+/// </summary>
+public static class MD5Helper
+{
+    /// <summary>
+    /// 对指定路径的文件加密，返回加密后的文本
+    /// </summary>
+    /// <param name="filePath"></param>
+    /// <returns></returns>
+    public static string MD5EncryptFile(string filePath)
+    {
+        
+    }
+
+
+    /// <summary>
+    /// 对指定的字符串加密，返回加密后的字符串
+    /// </summary>
+    /// <param name="originStr"></param>
+    /// <returns></returns>
+    public static string MD5EncryptString(string originStr)
+    {
+        
+    }
+}
