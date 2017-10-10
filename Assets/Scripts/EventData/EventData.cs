001940b975d288dd5e0024f4c4ea06d322b19f36
马三小伙儿 <1790617178@qq.com>
2017-10-10 18:11:52 +0800

    事件数据

---
+++ Assets/Scripts/EventData/EventData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 接收消息后触发的回调
+/// </summary>
+/// <param name="data"></param>
+public delegate void MsgHander(MsgHandlerData data);
+
+/// <summary>
+/// 事件处理器的接口
+/// </summary>
+public interface IEventHandler
+{
+    bool HandleMessage(GameEvent evt);
+
+    bool IsHasHandler(GameEvent evt);
+}
+
+/// <summary>
+/// 事件消息传递的数据
+/// </summary>
+public class MsgHandlerData
+{
+    
+}
+
+/// <summary>
+/// 游戏中的事件
+/// </summary>
+public class GameEvent
+{
+    
+}
