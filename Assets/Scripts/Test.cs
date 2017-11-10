a252b20513b4096ea2599af1469ee7f22f7f6ffc
马三小伙儿 <1790617178@qq.com>
2017-11-10 17:43:54 +0800

    添加测试模块

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    //TimeHelper.SetTimer(() => { Debug.LogError("时间到！"); }, 1f, false);
+	    TimeHelper.SetRepeatTimer(() => { Debug.Log("1s打印一次"); }, 0.5f);
+        //InvokeRepeating("Log",1f,1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
