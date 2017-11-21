454e45a40f0d10079691ef8bfcabe3aed38286bc
马三小伙儿 <1790617178@qq.com>
2017-11-21 19:17:45 +0800

    资源加载助手类

---
+++ Assets/Scripts/CommonHelper/ResourceLoader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Object =UnityEngine.Object;
+
+/// <summary>
+/// 资源加载助手类
+/// </summary>
+public class ResourceLoader : MonoBehaviour {
+
+    /// <summary>
+    /// 根据类型和路径返回相应的资源
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    public T Load<T>(string path) where T : Object
+    {
+        return Resources.Load<T>(path);
+    }
+
+    /// <summary>
+    /// 根据类型和路径返回相应的资源
+    /// </summary>
+    /// <param name="path"></param>
+    /// <param name="type"></param>
+    /// <returns></returns>
+    public Object Load(string path, Type type)
+    {
+        return Resources.Load(path, type);
+    }
+}
