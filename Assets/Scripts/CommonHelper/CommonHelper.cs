d485a8c68e15978c18cf9d752bcb55b536abc2e2
马三小伙儿 <1790617178@qq.com>
2017-12-04 19:59:27 +0800

    按钮添加事件通用方法

--- Assets/Scripts/CommonHelper/CommonHelper.cs
+++ Assets/Scripts/CommonHelper/CommonHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -17,5 +18,51 @@ public class CommonHelper
     public static string GetText(int id)
     {
         return I18NHelper.GetInstance().GetI18NText(id);
+    }
+
+    /// <summary>
+    /// 给按钮添加点击事件
+    /// </summary>
+    /// <param name="go"></param>
+    /// <param name="callback"></param>
+    public static void AddBtnMsg(GameObject go, Action<GameObject> callback)
+    {
+        
+    }
+
+    /// <summary>
+    /// 变更一个按钮的点击事件
+    /// </summary>
+    /// <param name="go"></param>
+    /// <param name="callback"></param>
+    public static void ChangeBtnMsg(GameObject go, Action<GameObject> callback)
+    {
+        
+    }
+
+    /// <summary>
+    /// 删除一个按钮的点击事件
+    /// </summary>
+    /// <param name="go"></param>
+    /// <param name="callback"></param>
+    public static void RemoveBtnMsg(GameObject go, Action<GameObject> callback)
+    {
+
+    }
+
+    public static GameObject InstantiateGoByPath(string path, GameObject parent)
+    {
+        ResourceMgr mgr = ResourceMgr.GetInstance();
+        GameObject prefab = null;
+
+        if (null != mgr)
+        {
+            
+        }
+        else
+        {
+            Debug.LogWarning("检查资源管理器！");
+            return null;
+        }
     }
 }
