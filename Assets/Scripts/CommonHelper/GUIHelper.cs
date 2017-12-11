958bfc0e1ad283fd569d54942f60014e6167e872
马三小伙儿 <1790617178@qq.com>
2017-12-11 21:09:14 +0800

    GUIHelper

--- Assets/Scripts/CommonHelper/GUIHelper.cs
+++ Assets/Scripts/CommonHelper/GUIHelper.cs
@@ -2,15 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GUIHelper : MonoBehaviour {
+/// <summary>
+/// GUI工具类
+/// </summary>
+public class GUIHelper
+{
+    /// <summary>
+    /// UI画布的根节点
+    /// </summary>
+    private static GameObject uiRootObj = null;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    /// <summary>
+    /// 相机节点
+    /// </summary>
+    private static GameObject uiCameraObj = null;
+
+    /// <summary>
+    /// UI相机
+    /// </summary>
+    private static Camera uiCamera = null;
+
+    /// <summary>
+    /// UI画布
+    /// </summary>
+    private static Canvas uiRoot = null;
+
+    /// <summary>
+    /// 3D物体的根节点
+    /// </summary>
+    private static GameObject ui3DRootObj = null;
+
+    private static void UGUICreate()
+    {
+        if (null == uiRootObj)
+        {
+            //创建画布根节点，相机节点，3D物体根节点
+            int uiLayer = LayerMask.NameToLayer("UI");
+            GameObject rootObj = new GameObject("UGUIRoot");
+            rootObj.layer = uiLayer;
+
+            ui3DRootObj = new GameObject("ui3DRoot");
+            ui3DRootObj.transform.parent = rootObj.transform;
+            ui3DRootObj.layer = LayerMask.NameToLayer("UI_Model");
+
+            uiRootObj = new GameObject("Canvas");
+        }
+    }
 }
