0cd38d7caf1ecf2fdd8f8166436f85ccd1de5890
马三小伙儿 <1790617178@qq.com>
2017-12-15 22:03:03 +0800

    资源路径信息类

---
+++ Assets/Scripts/Data/ResPathData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// 资源路径信息类
+/// </summary>
+class ResPathData:LocalDataBase
+{
+    public override void InitWithStr(string strData, char splitChar = ',')
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+/// <summary>
+/// ResPathData的数据集类
+/// </summary>
+class ResPathDataMap : ILocalDataMapBase
+{
+    public void SetMapCsv(string[] rows)
+    {
+        throw new NotImplementedException();
+    }
+}
