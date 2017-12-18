c509727a49c1a525156d246aadc1b6fa06957a3c
马三小伙儿 <1790617178@qq.com>
2017-12-18 17:53:47 +0800

    启动器脚本

---
+++ Assets/Scripts/GameLauncher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLauncher : MonoBehaviour {
+
+    void Awake()
+    {
+
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
