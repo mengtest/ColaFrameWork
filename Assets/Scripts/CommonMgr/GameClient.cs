f25dc3a22b60c273d7576f2fe33c79afe6fda147
马三小伙儿 <1790617178@qq.com>
2017-12-19 23:27:41 +0800

    游戏客户端Module类

---
+++ Assets/Scripts/CommonMgr/GameClient.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Game客户端Module类，处理很多核心逻辑
+/// </summary>
+public class GameClient
+{
+    /// <summary>
+    /// 获取当前游戏客户端的版本号
+    /// </summary>
+    /// <returns></returns>
+    public static string GetClientVersion()
+    {
+        return "1.0.0";
+    }
+}
