1a26f2746c4f28de95dbc268d1833357618f6204
马三小伙儿 <1790617178@qq.com>
2017-12-23 18:33:42 +0800

    UIBase类

--- Assets/Scripts/UIBase/IViewBase.cs
+++ Assets/Scripts/UIBase/IViewBase.cs
@@ -2,15 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IViewBase : MonoBehaviour {
+/// <summary>
+/// UI的接口
+/// </summary>
+public interface IViewBase
+{
+    
+}
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+/// <summary>
+/// UI基类
+/// </summary>
+public abstract class UIBase : IViewBase
+{
+    /// <summary>
+    /// 当前的UI界面GameObject
+    /// </summary>
+    public GameObject Panel { get; protected set; }
+
+    /// <summary>
+    /// 当前的UI界面GameObject对应的唯一资源ID
+    /// </summary>
+    public int ResId { get; protected set; }
+
+    /// <summary>
+    /// UI界面的名字
+    /// </summary>
+    public string Name { get; protected set; }
+
+    /// <summary>
+    /// UI界面的深度
+    /// </summary>
+    public int Depth { get; set; }
+
+    /// <summary>
+    /// UI界面的层级
+    /// </summary>
+    public int Layer { get; set; }
 }
