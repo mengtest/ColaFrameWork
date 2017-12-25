4a3778c154a1388c9182946af6005bb495088710
马三小伙儿 <1790617178@qq.com>
2017-12-25 16:07:33 +0800

    接口与基类拆分

---
+++ Assets/Scripts/UIBase/UIBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// UI基类
+/// </summary>
+public abstract class UIBase : IViewBase
+{
+    /// <summary>
+    /// 当前的UI界面GameObject
+    /// </summary>
+    public GameObject Panel { get; protected set; }
+
+    /// <summary>
+    /// 当前的UI界面GameObject对应的唯一资源ID
+    /// </summary>
+    public int ResId { get; protected set; }
+
+    /// <summary>
+    /// UI界面的名字
+    /// </summary>
+    public string Name { get; protected set; }
+
+    /// <summary>
+    /// UI界面的深度
+    /// </summary>
+    public int Depth { get; set; }
+
+    /// <summary>
+    /// UI界面的层级
+    /// </summary>
+    public int Layer { get; set; }
+}
